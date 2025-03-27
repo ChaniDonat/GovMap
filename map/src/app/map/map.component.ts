939758363f72449dfbf0a7552b7b565088c24fa1
Chani Donat <chani72901@gmail.com>
2025-03-27 10:17:04 +0200

    get getCoordinates by address

--- map/src/app/map/map.component.ts
+++ map/src/app/map/map.component.ts
@@ -17,14 +17,6 @@ export class MapComponent {
   async ngOnInit() {
 
     await this.svc.init();
-    const options = {
-      token: '5a4b8472-b95b-4687-8179-0ccb621c7990',//לצורך בדיקות
-      // '2596da32-3178-4a04-b596-aaaf9a9df2df',
-      visibleLayers: ["cell_active", "bus_stops", "SUB_GUSH_ALL", "PARCEL_ALL"],
-      layers: ["cell_active", "bus_stops", "SUB_GUSH_ALL", "PARCEL_ALL", "parcel_all"],
-      showXY: true,
-      identifyOnClick: true,
-    };
-    const map = this.svc.createMapIframe('map');// new GovMap.createMap('map', options);
+    const map = this.svc.createMapIframe('map');
   }
 }
--- map/src/app/table/table.component.ts
+++ map/src/app/table/table.component.ts
@@ -12,17 +12,157 @@ import { log } from 'console';
   styleUrl: './table.component.css'
 })
 export class TableComponent {
-  tableData = [
-    { name: ' שאדיקר נחום', x: 219730, y: 635514, col4: 'Data 1', col5: 'Data 2', col6: 'Data 3', col7: 'Data 4', col8: 'Data 5' },
-    { name: 'ירושלים', x: 219832, y: 635857, col4: 'Data 6', col5: 'Data 7', col6: 'Data 8', col7: 'Data 9', col8: 'Data 10' },
-    { name: 'חיפה', x: 221330, y: 633930, col4: 'Data 11', col5: 'Data 12', col6: 'Data 13', col7: 'Data 14', col8: 'Data 15' },
-    { name: 'ירושלים', x: 221157, y: 629637, col4: 'Data 6', col5: 'Data 7', col6: 'Data 8', col7: 'Data 9', col8: 'Data 10' },
-    { name: 'ירושלים', x: 221393, y: 633431, col4: 'Data 6', col5: 'Data 7', col6: 'Data 8', col7: 'Data 9', col8: 'Data 10' },
-    { name: 'ירושלים', x: 220201, y: 630588, col4: 'Data 6', col5: 'Data 7', col6: 'Data 8', col7: 'Data 9', col8: 'Data 10' },
-    { name: 'תל אביב', x: 216721, y: 630524, col4: 'Data 6', col5: 'Data 7', col6: 'Data 8', col7: 'Data 9', col8: 'Data 10' },
-    { name: 'ירושלים', x: 219766, y: 635698, col4: 'Data 6', col5: 'Data 7', col6: 'Data 8', col7: 'Data 9', col8: 'Data 10' },
-
-  ];
+  data = {
+    Yeshuv: 3000,
+    Street: null,
+    Home: null,
+    PropertyTypeList: null,
+    FromDateIska: null,
+    ToDateIska: null,
+    MahutIskaList: null,
+    OldMahutIskaList: null,
+    FromShoviMeshuarach: null,
+    ToShoviMeshuarach: null,
+    RoomsList: null,
+    FromYear: null,
+    ToYear: null,
+    FromArea: null,
+    ToArea: null,
+    outArrData: [
+      {
+        Isn: 2153933.0,
+        ShmGushHelkaTat: "31267-23-5",
+        ShmYeshuv: "3000",
+        ShmYomMechira: 20241029,
+        ShmShoviIska: 1231229.0,
+        shmShoviMuzhar: 1231229.0,
+        ShmChalakim: 1.0,
+        ShmShnatBnia: 2024,
+        ShmShetch: 84,
+        ShmSugNeches: 1,
+        ShmMahutIska: 101,
+        ShmSwCarmen: 1,
+        ShmMone: 1,
+        ShmMechane: 1,
+        ShmSugHishuv: 50,
+        ShmSugHishuvFlag: null,
+        CorX: 219730.0,
+        CorY: 635514.0,
+        ShmRehov: "ירושלים - ללא רחוב",
+        ShmMisparBayit: "12"
+      },
+      {
+        Isn: 3568121.0,
+        ShmGushHelkaTat: "30043-14-51",
+        ShmYeshuv: "3000",
+        ShmYomMechira: 20241014,
+        ShmShoviIska: 361111.0,
+        shmShoviMuzhar: 361111.0,
+        ShmChalakim: 0.1111111,
+        ShmShnatBnia: 1960,
+        ShmShetch: 88,
+        ShmSugNeches: 1,
+        ShmMahutIska: 101,
+        ShmSwCarmen: 1,
+        ShmMone: 2,
+        ShmMechane: 18,
+        ShmSugHishuv: 495,
+        ShmSugHishuvFlag: null,
+        CorX: null,
+        CorY: null,
+        ShmRehov: "טרומפלדור",
+        ShmMisparBayit: "1"
+      },
+      {
+        Isn: 3430702.0,
+        ShmGushHelkaTat: "30616-214-0",
+        ShmYeshuv: "3000",
+        ShmYomMechira: 20241009,
+        ShmShoviIska: 427272.72,
+        shmShoviMuzhar: 427272.0,
+        ShmChalakim: 0.0909090,
+        ShmShnatBnia: 0,
+        ShmShetch: 0,
+        ShmSugNeches: 1,
+        ShmMahutIska: 0,
+        ShmSwCarmen: 0,
+        ShmMone: 1,
+        ShmMechane: 11,
+        ShmSugHishuv: 495,
+        ShmSugHishuvFlag: null,
+        CorX: 219832.0,
+        CorY: 635857.0,
+        ShmRehov: "",
+        ShmMisparBayit: ""
+      },
+      {
+        Isn: 3278762.0,
+        ShmGushHelkaTat: "30013-71-1",
+        ShmYeshuv: "3000",
+        ShmYomMechira: 20241001,
+        ShmShoviIska: 834667.0,
+        shmShoviMuzhar: 834667.0,
+        ShmChalakim: 0.1666666,
+        ShmShnatBnia: 0,
+        ShmShetch: 0,
+        ShmSugNeches: 1,
+        ShmMahutIska: 101,
+        ShmSwCarmen: 0,
+        ShmMone: 1,
+        ShmMechane: 6,
+        ShmSugHishuv: 495,
+        ShmSugHishuvFlag: null,
+        CorX: 221049.0,
+        CorY: 629352.0,
+        ShmRehov: "",
+        ShmMisparBayit: ""
+      },
+      {
+        Isn: 3082565.0,
+        ShmGushHelkaTat: "30013-29-2",
+        ShmYeshuv: "3000",
+        ShmYomMechira: 20240924,
+        ShmShoviIska: 730000.0,
+        shmShoviMuzhar: 730000.0,
+        ShmChalakim: 0.2000000,
+        ShmShnatBnia: 0,
+        ShmShetch: 0,
+        ShmSugNeches: 1,
+        ShmMahutIska: 101,
+        ShmSwCarmen: 0,
+        ShmMone: 1,
+        ShmMechane: 5,
+        ShmSugHishuv: 495,
+        ShmSugHishuvFlag: null,
+        CorX: 221157.0,
+        CorY: 629637.0,
+        ShmRehov: "",
+        ShmMisparBayit: ""
+      },
+      {
+        Isn: 3620035.0,
+        ShmGushHelkaTat: "30408-121-2",
+        ShmYeshuv: "3000",
+        ShmYomMechira: 20240822,
+        ShmShoviIska: 5008000.0,
+        shmShoviMuzhar: 5008000.0,
+        ShmChalakim: 0.9700000,
+        ShmShnatBnia: 0,
+        ShmShetch: 0,
+        ShmSugNeches: 1,
+        ShmMahutIska: 105,
+        ShmSwCarmen: 0,
+        ShmMone: 97,
+        ShmMechane: 100,
+        ShmSugHishuv: 492,
+        ShmSugHishuvFlag: null,
+        CorX: 216721.0,
+        CorY: 630524.0,
+        ShmRehov: "מרטון יחזקאל",
+        ShmMisparBayit: "3"
+      },
+    ],
+  };
 
   constructor(private govMapService: GovMapService) { }
 
@@ -30,22 +170,34 @@ export class TableComponent {
     this.govMapService.mapReady$.subscribe(async (isReady) => {
       if (isReady) {
         // this.govMapService.zoom(this.tableData[0].x, this.tableData[0].y, 8);
-        this.govMapService.showPoints(this.tableData)
+        this.govMapService.showPoints(this.data.outArrData)
         // this.govMapService.zoom(220218, 631937, 8);
         // 220218.58,631937.52
-        let point=this.govMapService.getCoordinates(" שאדיקר נחום ירושלים");
-        this.govMapService.zoom((await point).corX, (await point).corY,8);
+        let point = this.govMapService.getCoordinates(this.data.Yeshuv.toString());
+        this.govMapService.zoom((await point).corX, (await point).corY, 8);
       }
     });
   }
 
-  onMouseEnter(row: any) {
-    // שולח את הערכים למפה כאשר העכבר עובר על שורה
-    this.govMapService.showCustomBubble(row.x, row.y);
-    let filterData = this.tableData.filter((d: any) => d != row)
-    this.govMapService.showPoints(filterData)
-    // this.govMapService.zoom(row.x, row.y, 8);
+  async onMouseEnter(row: any) {
+    debugger
+    if (row.CorX == null || row.CorY == null) {
+      //מה לעשות במקרה ואין לי שם רחוב????
+      if (row.ShmRehov.length) {
+        let point = this.govMapService.getCoordinates(`ירושלים ${row.ShmRehov} ${row.ShmMisparBayit}`)
+        row.corX = (await point).corX;
+        row.corX = (await point).corY;
+       this.govMapService.showCustomBubble((await point).corX, (await point).corY,row);
+      }
+    }
+    else {
+      // שולח את הערכים למפה כאשר העכבר עובר על שורה
+      this.govMapService.showCustomBubble(row.CorX, row.CorY,row);
+      let filterData = this.data.outArrData.filter((d: any) => d != row)
+      this.govMapService.showPoints(filterData)
+      // this.govMapService.zoom(row.x, row.y, 8);
 
 
+    }
   }
 }
